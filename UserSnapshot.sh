dae486bd7e956b015b101a83f0bfe3d86dbde1bb
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 17:16:22 -0500

    Create UserSnapshot.sh

---
+++ UserSnapshot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+## Take snapshots of usernames each hour for comparison.
+#
+
+cronjob -e 0 24 0 0 0 /etc/passwd > /var/log/username_snap%0.log
+diff -ur /etc/passwd /var/log/usernamesnap%0.log > usernamesnap%0.log
+
+# END
